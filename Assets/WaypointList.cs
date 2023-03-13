70bb1a72ab16c215ce33f7045171b523534c44e9
Justin <Justin McIntyre>
2023-03-12 22:15:12 -0300

    finished AddWaypoint

--- Assets/WaypointList.cs
+++ Assets/WaypointList.cs
@@ -5,18 +5,19 @@ using UnityEngine.Events;
 using static GridSystem;
 using static GridManager;
 using static Cell;
+using static WaypointNode;
 
 
 public class WaypointList{
-private WaypointNode head;
-private List<WaypointNode> deadends;
-private WaypointNode currentnode;
+    private WaypointNode head;
+    private List<WaypointNode> deadends;
+    private WaypointNode currentnode;
 
     public WaypointList(){
-    var head = new WaypointNode(0,0);
-    this.head = head;
-    this.deadends.Add(head);
-    this.currentnode = head;
+        var head = new WaypointNode(0,0);
+        this.head = head;
+        this.deadends.Add(head);
+        this.currentnode = head;
     }
 
     public AddWaypoint(int x, int y){
@@ -30,17 +31,22 @@ private WaypointNode currentnode;
 
         var pastx = deadend.nodecoordinate.x;
         var newx = pastx+x;
-
+        var deadend = this.deadends[0];
         if(this.deadends[0].previousnodes.length != 0){
 
-            var deadend = this.deadends[0]
             
-            var previousnode = deadends.previousnodes[0];
-            if(deadend.nodecoordinate.x != previousnode.nodecoordinate.x){
-                Math.Abs(deadend.nodecoordinate.x + previousnode.nodecoordinate.x) < Math.Abs(deadend.nodecoordinate.x + newx);
+            var deadendsPreviousNode = deadends.previousnodes[0];
+            if(deadend.nodecoordinate.x != deadendsPreviousNode.nodecoordinate.x){
+                if(Math.Clamp(newx - deadend.nodecoordinate.x) == Math.Clamp(deadendsPreviousNode.nodecoordinate.x - deadend.nodecoordinate.x)) {
+                    throw new Exception("Cannot backtrack");
+                }
             }
         }
-        
+
+        WaypointNode newWaypointNode = new WaypointNode(newx, deadends.nodecoordinate.y);
+        newWaypointNode.previousnodes.Add(deadend);
+        deadend.forwardnodes.Add(newWaypointNode);
+        this.deadends[0] = newWaypointNode;
     }
 
 }
